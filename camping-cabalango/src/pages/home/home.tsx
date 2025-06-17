15ba0f37896c32014a6faebb2a19a786db970989
SaraJoLo <162146662+SaraJoLo@users.noreply.github.com>
2025-06-17 10:42:17 -0300

    secrion services

--- camping-cabalango/src/pages/home/home.tsx
+++ camping-cabalango/src/pages/home/home.tsx
@@ -1,9 +1,43 @@
 import './home.scss';
 import campingImg from '../../assets/img/camping/1111.jpg';
+import { GiBarbecue } from 'react-icons/gi';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faWifi,
+  faPaw,
+  faCar,
+  faRecycle,
+  faUtensils,
+  faCampground,
+  faPersonSwimming,
+  faStar,
+  faHotel,
+  faPersonHiking,
+} from '@fortawesome/free-solid-svg-icons';
+import { PiTree } from 'react-icons/pi';
+
+
+
+const amenities = [
+  { icon: <FontAwesomeIcon icon={faRecycle} />, label: "Sostenibilidad y reciclaje" },
+  { icon: <FontAwesomeIcon icon={faWifi} />, label: "Wi-Fi" },
+  { icon: <FontAwesomeIcon icon={faCar} />, label: "Estacionamiento" },
+  { icon: <FontAwesomeIcon icon={faPaw} />, label: "Admite mascotas (con correa)" },
+  { icon: <GiBarbecue />, label: "Zona de barbacoa" },
+  { icon: <FontAwesomeIcon icon={faUtensils} />, label: "Mesas, sillas y bancos" },
+  { icon: <FontAwesomeIcon icon={faCampground} />, label: "Espacio para acampar" },
+  { icon: <PiTree/>, label : "Áreas verdes y sombra" },
+  { icon: <FontAwesomeIcon icon={faPersonSwimming} />, label: "Río con ollas y playa" },
+  { icon: <FontAwesomeIcon icon={faStar} />, label: "Luz de noche" },
+  { icon: <FontAwesomeIcon icon={faHotel} />, label: "Albergue de montaña" },
+  { icon: <FontAwesomeIcon icon={faPersonHiking} />, label: "Actividades recreativas" },
+];
+
+
 
 const Home = () => {
   return (
-    <main className="home">
+    <section className="home">
       <section className="home__hero">
          <div className="home__content">
           <h1>Camping Cabalango</h1>
@@ -16,7 +50,20 @@ const Home = () => {
           <img src={campingImg} alt="Vista del camping Cabalango" />
         </div>
       </section>
-    </main>
+      <section className="services">
+          <div className="icon-section">
+            <h3>SERVICIOS</h3>
+            <div className="amenities-grid">
+             {amenities.map((item, index) => (
+                <div className="amenity-item" key={index}>
+                  <div className="amenity-icon">{item.icon}</div>
+                  <span className="amenity-text">{item.label}</span>
+                </div>
+              ))}
+            </div>
+          </div>
+      </section>
+    </section>
   );
 };
 
